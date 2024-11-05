0313d7bad09cd98142420f92f9fced1838be0d98
isaiah0311 <isaiahl0311@gmail.com>
2024-11-04 16:31:57 -1000

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * \file main.c
+ * \author Isaiah Lateer
+ * 
+ * Calculates if a given year is a leap year or not.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Entry point for the program.
+ * 
+ * \return Exit code.
+ */
+int main() {
+    printf("Leap Year Calculator\n");
+    return EXIT_SUCCESS;
+}
